5a06595b0868cb9c532202659da8a4c0dd45f36a
John Hart <john.hart003@student.swic.edu>
2025-02-27 23:37:32 -0600

    Add function practice examples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-const eightBall = ['Ball', 8, true,];
+const student1 = {
+	name: "John",
+	course: "Web Development 1",
+	grade: 90,
+};
+const student2 = {
+	name: "Matt",
+	course: "Python 2",
+	grade: 86,
+};
+const student3 = {
+	name: "Alice",
+	course: "C#",
+	grade: 97,
+};
+function displayStudentInfo(student) {
+	return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%`;
+}
 
-console.log(eightBall[3]);
+console.log(displayStudentInfo(student1));
+console.log(displayStudentInfo(student2));
+console.log(displayStudentInfo(student3));
