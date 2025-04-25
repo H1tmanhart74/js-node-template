6fabde9644df6e2cf9cb0435fafafedb458f02eb
John Hart <john.hart003@student.swic.edu>
2025-04-24 21:18:48 -0500

    feat: Add new fetch data using then

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-const student1 = {
-	name: "John",
-	course: "Web Development 1",
-	grade: 90,
-};
-const student2 = {
-	name: "Matt",
-	course: "Python 2",
-	grade: 86,
-};
-const student3 = {
-	name: "Alice",
-	course: "C#",
-	grade: 97,
-};
-function displayStudentInfo(student) {
-	return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%`;
-}
-
-console.log(displayStudentInfo(student1));
-console.log(displayStudentInfo(student2));
-console.log(displayStudentInfo(student3));
+fetch("https://www.swapi.tech/api/people/2")
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data);
+	});
